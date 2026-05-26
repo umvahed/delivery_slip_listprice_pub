3d386cf621ec65b08dba736e756097f46a5d1480
Ahmed Vahed <ahmedvahed@gmail.com>
2026-05-27 00:57:59 +0200

    fix: adjust price to EUR 1.99

--- delivery_slip_listprice/__manifest__.py
+++ delivery_slip_listprice/__manifest__.py
@@ -28,7 +28,7 @@ Related module: **Delivery Slip Unit Cost** (shows cost price instead).
         'report/report_deliveryslip_listprice.xml',
     ],
     'images': ['static/description/banner.png'],
-    'price': 15.00,
+    'price': 1.99,
     'currency': 'EUR',
     'installable': True,
     'auto_install': False,
